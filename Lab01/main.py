e891a12739d7ec787f55fbfd21954c6041bf35bb
fvolar00 <109236937+fvolar00@users.noreply.github.com>
2023-07-02 21:55:38 +0200

    Update main.py

--- Lab01/main.py
+++ Lab01/main.py
@@ -1,19 +1,3 @@
-# import hashlib
-
-# # Set the path to the USB image file
-# image_path = 'C:\\Users\\A508\\lnikol01\\lab1\\imageFESB.001'
-
-# given_hash = "201cdee056cfc8c0996328e3c2115b513a141f5c"
-
-# # Compute the SHA-1 hash of the bitstream image
-# with open(image_path, 'rb') as f:
-#     image_hash = hashlib.sha1(f.read()).hexdigest()
-
-# # Compare the hashes to verify the integrity of the image
-# if given_hash == image_hash:
-#     print('Bitstream image verified successfully')
-# else:
-#     print('Error: bitstream image verification failed')
 
 import socket
 import subprocess
